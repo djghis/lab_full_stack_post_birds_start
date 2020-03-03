948ed9945e3c398c1b63a31bd525d5170eb1b434
djghis <djghis@hotmail.com>
2020-03-03 15:06:07 +0000

    initial commit

---
+++ client/src/services/SightingService.js
@@ -0,0 +1,22 @@
+const baseURL = 'http://localhost:3000/api/sightings/'
+
+export default {
+  getSightings(){
+    return fetch(baseURL)
+    .then(res => res.json())
+  },
+  addSighting(payload){
+    console.log("payload", payload);
+    return fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+  },
+  deleteSighting(id){
+    return fetch(baseURL + id, {
+      method: 'DELETE'
+    })
+  }
+}
